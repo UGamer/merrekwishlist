b11d47c1e0fe6bf92d485d6b146f6ee53a13dd22
UGamer <itsugamer@gmail.com>
2019-09-11 13:55:02 -0400

    (Overhaul) [Pre-Alpha] 1.0.0
    Started the overhaul. This time we will be using HTML, CSS, JavaScript, and JSON.
    
    Re-did the file structure.

---
+++ Wishlist/DatabaseScript.js
@@ -0,0 +1,10 @@
+var imageTitle;
+var title;
+var want;
+var price;
+var deliveryTime;
+var description;
+var url;
+
+$("data").append();
+document.getElementById("demo").innerHTML = "Paragraph changed.";
