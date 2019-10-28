091f12c0f9987d03392f234ef47647755569ff0a
UGamer <itsugamer@gmail.com>
2019-10-28 01:59:49 -0400

    [Beta] 1.0.0
    + Added items to the wishlist
    + The table is automatically constructed
    + Fixed the ' and " bug
    + Clicking on links now sends you to a new tab

--- Wishlist/SQLite to JSON/SQLite to JSON/SQLite to JSON/JSONResult.cs
+++ Wishlist/SQLite to JSON/SQLite to JSON/SQLite to JSON/JSONResult.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,23 +17,72 @@ namespace SQLite_to_JSON
         {
             string jsonString = "data = '[";
 
+            Regex rgxFix1 = new Regex("'");
+            Regex rgxFix2 = new Regex("\"");
             foreach (DataRow row in table.Rows)
             {
+                string imageTitle = row["ImageTitle"].ToString();
+                string title = row["Title"].ToString();
+                string want = row["Want"].ToString();
+                string price = row["Price"].ToString();
+                string deliveryTime = row["DeliveryTime"].ToString();
+                string description = row["Description"].ToString();
+                string url = row["URL"].ToString();
+
+                // This section replaces all 's and "s to work in the JSON string
+
+                imageTitle = rgxFix1.Replace(imageTitle, "\\\'");
+                imageTitle = rgxFix2.Replace(imageTitle, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in imageTitle");
+
+                    title = rgxFix1.Replace(title, "\\\'");
+                    title = rgxFix2.Replace(title, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in title");
+
+                    want = rgxFix1.Replace(want, "\\\'");
+                    want = rgxFix2.Replace(want, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in want");
+
+                    price = rgxFix1.Replace(price, "\\\'");
+                    price = rgxFix2.Replace(price, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in price");
+
+                    deliveryTime = rgxFix1.Replace(deliveryTime, "\\\'");
+                    deliveryTime = rgxFix2.Replace(deliveryTime, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in deliveryTime");
+
+                    description = rgxFix1.Replace(description, "\\\'");
+                    description = rgxFix2.Replace(description, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in description");
+
+                    url = rgxFix1.Replace(url, "\\\'");
+                    url = rgxFix2.Replace(url, "\\\'\\\'");
+
+                Console.WriteLine("Replaced stuff in url");
+
+                Console.WriteLine("Successfully fixed " + row["Title"].ToString());
+
                 jsonString += "{";
 
-                jsonString += "\"imageTitle\":\"" + row["ImageTitle"].ToString() + "\",";
+                jsonString += "\"imageTitle\":\"" + imageTitle + "\",";
 
-                jsonString += "\"title\":\"" + row["Title"].ToString() + "\",";
+                jsonString += "\"title\":\"" + title + "\",";
 
-                jsonString += "\"want\":\"" + row["Want"].ToString() + "\",";
+                jsonString += "\"want\":\"" + want + "\",";
 
-                jsonString += "\"price\":\"" + row["Price"].ToString() + "\",";
+                jsonString += "\"price\":\"" + price + "\",";
 
-                jsonString += "\"deliveryTime\":\"" + row["DeliveryTime"].ToString() + "\",";
+                jsonString += "\"deliveryTime\":\"" + deliveryTime + "\",";
 
-                jsonString += "\"description\":\"" + row["Description"].ToString() + "\",";
+                jsonString += "\"description\":\"" + description + "\",";
 
-                jsonString += "\"url\":\"" + row["URL"].ToString() + "\"},";
+                jsonString += "\"url\":\"" + url + "\"},";
             }
 
             jsonString = jsonString.Substring(0, jsonString.Length - 1);
--- Wishlist/script.js
+++ Wishlist/script.js
@@ -8,6 +8,19 @@ var url;
 
 function load() {
 	var mydata = JSON.parse(data);
-	alert(mydata[0].title);
-	alert(mydata[1].title);
+	
+	var index;
+	var code = "<table id=\"dataTable\"><tr id=\"heading\"><th></th><th style=\"width:15%;\">Title</th><th style=\"width:5%;\">Want Rating</th><th style=\"width:5%;\">Price</th><th style=\"width:5%;\">Delivery Time</th><th style=\"width:20%;\">Description</th></tr>";
+	for (index = 0; index < mydata.length; index++)
+	{
+		code += "<tr><td><a href=\"" + mydata[index].url + "\" target=\"_blank\"><img src=\"img\\Christmas2019\\" + mydata[index].imageTitle + ".png\" style=\"width=100%;\"></a></td>";
+		code += "<td style=\"width:15%;\">" + mydata[index].title + "</td>";
+		code += "<td style=\"width:5%;\">" + mydata[index].want + "</td>";
+		code += "<td style=\"width:5%;\">$" + mydata[index].price + "</td>";
+		code += "<td style=\"width:5%;\">" + mydata[index].deliveryTime + "</td>";
+		code += "<td style=\"width:20%;\">" + mydata[index].description + "</td></tr>";
+	}
+	
+	code += "</table>";
+	document.getElementById("data").innerHTML = code;
 }
